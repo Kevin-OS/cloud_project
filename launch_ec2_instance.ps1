2986969e2c1cbf4e1ad5906c560708d1ffcc0c40
U-Kevin-PC\Kevin <kevpcmaint@hotmail.co.uk>
2017-03-29 19:58:12 +0100

    Added code to create ec2 key pair

---
+++ launch_ec2_instance.ps1
@@ -0,0 +1,7 @@
+﻿"`n"
+echo "*****************Creating new key pair*****************`n"
+$keypairname = read-host -Prompt 'Please enter a name for a new key pair: '
+$keypair = New-EC2KeyPair -KeyName $keypairname
+$keypair.KeyMaterial | Out-File -Encoding ascii C:\Users\$env:USERNAME\Desktop\$keypairname.pem
+echo "`n*****************Key pair $keypairname created successfully*******************"
+"`n"
