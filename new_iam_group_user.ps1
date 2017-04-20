4cec53438559dd3ffd20ece63d96c9469e8fba91
U-Kevin-PC\Kevin <kevpcmaint@hotmail.co.uk>
2017-04-20 12:46:54 +0100

    Created script to create new iam group and user

---
+++ new_iam_group_user.ps1
@@ -0,0 +1,12 @@
+﻿echo "*****************Creating new IAM Group*****************`n"
+$groupname = read-host -Prompt 'Please enter a name for a IAM group: '
+New-IAMGroup -GroupName $groupname
+echo "*****************New IAM Group Created*****************`n"
+
+echo "*****************Creating new IAM User*****************`n"
+$iamuser = read-host -Prompt 'Please enter a name for a IAM user: '
+New-IAMUser -UserName $iamuser
+echo "*****************New IAM User Created*****************`n"
+
+Add-IAMUserToGroup -UserName $iamuser -GroupName $groupname
+echo "*****************$iamuser added to $groupname******************"
