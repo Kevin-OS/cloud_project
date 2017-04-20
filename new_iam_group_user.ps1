c0f116fc145c9d8c22e750efa86c70e53e84e9c7
U-Kevin-PC\Kevin <kevpcmaint@hotmail.co.uk>
2017-04-20 16:30:13 +0100

    Updated policy

--- new_iam_group_user.ps1
+++ new_iam_group_user.ps1
@@ -9,18 +9,38 @@ $policyDocument = @"
   "Statement": [
     {
       "Effect": "Allow",
-      "NotAction": "iam:*",
+      "NotAction": [
+                "iam:DeleteSSHPublicKey",
+                "iam:GetSSHPublicKey",
+                "iam:ListSSHPublicKeys",
+                "iam:UpdateSSHPublicKey",
+                "iam:UploadSSHPublicKey",
+                "iam:CreateServiceSpecificCredential",
+                "iam:ListServiceSpecificCredentials",
+                "iam:UpdateServiceSpecificCredential",
+                "iam:DeleteServiceSpecificCredential",
+                "iam:ResetServiceSpecificCredential",
+                "iam:GenerateCredentialReport",
+                "iam:GenerateServiceLastAccessedDetails",
+                "iam:Get*",
+                "iam:List*"
+                ],
       "Resource": "*"
     }
   ]
 }
 "@
+
 $timestamp = get-date -UFormat "%d-%m-%Y"
 Write-IAMGroupPolicy -GroupName $groupname -PolicyName "PowerUserAccess-$groupname-$timestamp" -PolicyDocument $policyDocument
 
 echo "*****************Creating new IAM User*****************`n"
+
 $iamuser = read-host -Prompt 'Please enter a name for an IAM user: '
 New-IAMUser -UserName $iamuser
+
+New-IAMLoginProfile -UserName $iamuser -Password "itt12345" -PasswordResetRequired $true
+
 echo "*****************New IAM User Created*****************`n"
 
 Add-IAMUserToGroup -UserName $iamuser -GroupName $groupname
