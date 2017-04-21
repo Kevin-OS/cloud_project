d3a79b9a0dba3f6d9df2d30f6b18ea6808cc4e76
U-Kevin-PC\Kevin <kevpcmaint@hotmail.co.uk>
2017-04-21 19:40:24 +0100

    Created script to create auto scaling group and scaling policies

---
+++ autoscalinggroup.ps1
@@ -0,0 +1,4 @@
+﻿$instanceid = Read-Host -prompt 'Enter Instance ID for Launch Configuration: '
+New-ASAutoScalingGroup -AutoScalingGroupName TestAutoScalingGroup -InstanceId $instanceid -MinSize 1 -MaxSize 3 -DesiredCapacity 2 -AvailabilityZone us-west-2b
+Write-ASScalingPolicy -AutoScalingGroupName TestAutoScalingGroup -PolicyName ScaleDown -ScalingAdjustment -1 -AdjustmentType ChangeInCapacity
+Write-ASScalingPolicy -AutoScalingGroupName TestAutoScalingGroup -PolicyName ScaleUp -ScalingAdjustment 1 -AdjustmentType ChangeInCapacity
