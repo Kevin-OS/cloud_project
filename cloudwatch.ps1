a840e12ef0eb1482474c0138d7cc25dd0d97f7f8
U-Kevin-PC\Kevin <kevpcmaint@hotmail.co.uk>
2017-04-21 19:40:47 +0100

    Created script to configure two cloudwatch alarms

---
+++ cloudwatch.ps1
@@ -0,0 +1,4 @@
+﻿$arn = Read-Host -Prompt 'Please enter arn for scale down policy: '
+Write-CWMetricAlarm -AlarmName cpuAlarm1 -AlarmAction $arn -ComparisonOperator LessThanOrEqualToThreshold -Dimension @{ Name="AutoScalingGroupName"; Value="TestAutoScalingGroup" } -period 60 -EvaluationPeriod 1 -Namespace AWS/EC2 -Statistic Average -Threshold 19 -Unit Percent -MetricName CPUUtilization
+$arn = Read-Host -Prompt 'Please enter arn for scale up policy: '
+Write-CWMetricAlarm -AlarmName cpuAlarm2 -AlarmAction $arn -ComparisonOperator GreaterThanOrEqualToThreshold -Dimension @{ Name="AutoScalingGroupName"; Value="TestAutoScalingGroup" } -period 60 -EvaluationPeriod 1 -Namespace AWS/EC2 -Statistic Average -Threshold 20 -Unit Percent -MetricName CPUUtilization
